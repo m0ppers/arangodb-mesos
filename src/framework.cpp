fada1781cf49b0811aa932e40a1c1ed372018944
Max Neunhoeffer <max@arangodb.com>
2015-06-29 08:44:52 +0200

    Switch default mode to cluster.

--- src/framework.cpp
+++ src/framework.cpp
@@ -103,7 +103,7 @@ int main (int argc, char** argv) {
   flags.add(&mode,
             "mode",
             "Mode of operation (standalone, cluster)",
-            "standalone");
+            "cluster");
 
   string role;
   flags.add(&role,
